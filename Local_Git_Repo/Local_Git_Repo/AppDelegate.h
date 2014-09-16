c381d3e8e6d66e2a0679d47291be255fd65c2955
lxy <lglucifer@126.com>
2014-09-16 23:08:35 +0800

    first commit

---
+++ Local_Git_Repo/Local_Git_Repo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Local_Git_Repo
+//
+//  Created by LiuXiaoyu on 9/16/14.
+//  Copyright (c) 2014 __Company__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Local_Git_Repo/Local_Git_Repo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Local_Git_Repo
+//
+//  Created by LiuXiaoyu on 9/16/14.
+//  Copyright (c) 2014 __Company__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
